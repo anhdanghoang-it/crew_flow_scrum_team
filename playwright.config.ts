7851fa9579a23c9f9ff19d42e6aa6976fc63fc6f
anhdang <danganh2710.it@gmail.com>
2025-11-26 09:35:38 +0000

    update config playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    
+    /* Run in headless mode for environments without X server */
+    // headless: true,
   },
 
   /* Configure projects for major browsers */
